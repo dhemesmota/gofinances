55d4e97fc4369ef54a73d9a13cd0e561cd72cb24
Dhemes Mota <dhemes.mota@gmail.com>
2020-05-04 00:01:33 -0300

    Finalizando layout

--- mobile/src/pages/Register/index.tsx
+++ mobile/src/pages/Register/index.tsx
@@ -1,18 +1,75 @@
-import React from 'react';
+import React, { useState, useCallback } from 'react';
+import { Image } from 'react-native';
 
+import entradasImg from '../../assets/entradas.png';
+import saidasImg from '../../assets/saidas.png';
 import Header from '../../components/Header';
 
-import { Container, Content, Title } from './styles';
+import {
+  Container,
+  Content,
+  Title,
+  Form,
+  Input,
+  Button,
+  ButtonText,
+  ButtonsContent,
+  Btn,
+  BtnText,
+} from './styles';
 
-const Import: React.FC = () => {
+const Register: React.FC = () => {
+  const [selectButton, setSelectButton] = useState<
+    'income' | 'outcome' | 'default'
+  >('default');
+
+  const handleSubmit = useCallback((): void => {
+    console.log('submit');
+  }, []);
+
   return (
     <Container>
       <Header />
       <Content>
-        <Title>Cadastrar</Title>
+        <Title>Cadastro</Title>
+        <Form>
+          <Input placeholder="Nome" autoCapitalize="words" autoCorrect />
+          <Input placeholder="Preço" keyboardType="decimal-pad" />
+          <ButtonsContent>
+            <Btn
+              onPress={() => setSelectButton('income')}
+              select={selectButton === 'income'}
+              type={selectButton}>
+              <Image
+                source={entradasImg}
+                style={{ height: 26, width: 26, marginRight: 10 }}
+              />
+              <BtnText>Income</BtnText>
+            </Btn>
+            <Btn
+              onPress={() => setSelectButton('outcome')}
+              select={selectButton === 'outcome'}
+              type={selectButton}
+              style={{ marginLeft: 10 }}>
+              <Image
+                source={saidasImg}
+                style={{ height: 26, width: 26, marginRight: 10 }}
+              />
+              <BtnText>Outcome</BtnText>
+            </Btn>
+          </ButtonsContent>
+          <Input
+            placeholder="Categoria"
+            autoCapitalize="words"
+            autoCorrect={false}
+          />
+          <Button onPress={handleSubmit}>
+            <ButtonText>Enviar</ButtonText>
+          </Button>
+        </Form>
       </Content>
     </Container>
   );
 };
 
-export default Import;
+export default Register;
--- mobile/src/pages/Register/styles.ts
+++ mobile/src/pages/Register/styles.ts
@@ -1,6 +1,23 @@
-import styled from 'styled-components/native';
+import styled, { css } from 'styled-components/native';
 
-export const Container = styled.View`
+interface PropsBTN {
+  select?: boolean;
+  type: 'income' | 'outcome' | 'default';
+}
+
+const colorButtonSelect = {
+  income: css`
+    background: #d0dfd7;
+    border-color: #d0dfd7;
+  `,
+  outcome: css`
+    background: #e6d5d8;
+    border-color: #e6d5d8;
+  `,
+  default: '',
+};
+
+export const Container = styled.ScrollView`
   flex: 1;
 `;
 
@@ -9,4 +26,73 @@ export const Content = styled.View`
   padding: 25px;
 `;
 
-export const Title = styled.Text``;
+export const Title = styled.Text`
+  color: #000;
+  font-family: 'Poppins-Regular';
+  font-size: 20px;
+  line-height: 30px;
+  margin-bottom: 25px;
+`;
+
+export const Form = styled.View`
+  flex: 1;
+`;
+
+export const Input = styled.TextInput`
+  width: 100%;
+  height: 50px;
+  border-radius: 5px;
+  font-family: 'Poppins-Regular';
+  background: #fff;
+  color: #969cb3;
+  padding: 0 16px;
+  flex-direction: row;
+  align-items: center;
+  font-size: 14px;
+  line-height: 21px;
+  margin-bottom: 16px;
+`;
+
+export const Button = styled.TouchableOpacity`
+  background: #ff872c;
+  height: 50px;
+  width: 100%;
+  border-radius: 5px;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const ButtonText = styled.Text`
+  color: #fff;
+  font-family: 'Poppins-Regular';
+  font-size: 14px;
+  line-height: 21px;
+`;
+
+export const ButtonsContent = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const Btn = styled.TouchableOpacity<PropsBTN>`
+  background: transparent;
+  height: 50px;
+  flex: 1;
+  border-radius: 5px;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  border: 1.5px solid #e7e8ee;
+  margin-bottom: 16px;
+
+  ${(props) => props.select && colorButtonSelect[props.type]}
+`;
+
+export const BtnText = styled.Text`
+  color: #363f5f;
+  font-family: 'Poppins-Regular';
+  font-size: 14px;
+  line-height: 21px;
+`;
